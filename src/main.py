84eea1680dbe50ce4cfe6bce654a6db220c67280
Nataliia Novozhilova <belka1972@inbox.ru>
2025-12-01 16:11:52 +0300

    filter for english names

--- src/main.py
+++ src/main.py
@@ -31,6 +31,15 @@ def filter_russian_names(names_list: list) -> list:
     return new_name_list
 
 
+def filter_english_names(names_list: list) -> list:
+    """Функция принимает на вход список имен и выводит только английские имена"""
+    new_name_list = list()
+    for name_item in names_list:
+        if not is_cyrillic(name_item):
+            new_name_list.append(name_item)
+    return new_name_list
+
+
 def save_to_file(file_name: str, data: str) -> None:
     """Сохраняем имена в файл"""
     with open('data/' + file_name, 'w') as names_file:
@@ -42,4 +51,7 @@ if __name__ == '__main__':
 
     filtered_names = filter_russian_names(cleared_names)
     save_to_file('russian_names.txt', '\n'.join(filtered_names))
+
+    filtered_names = filter_english_names(cleared_names)
+    save_to_file('english_names.txt', '\n'.join(filtered_names))
 
