de9c0abb5eafc4a8d54d29aeb44c48e1a2dcf116
Nataliia Novozhilova <belka1972@inbox.ru>
2025-12-01 14:29:42 +0300

    file for russian names

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 import re
+from os import write
+
 
 def clear_names(file_name: str) -> list:
     """Функция по очистке списка имен от лишних символов"""
@@ -29,9 +31,15 @@ def filter_russian_names(names_list: list) -> list:
     return new_name_list
 
 
+def save_to_file(file_name: str, data: str) -> None:
+    """Сохраняем имена в файл"""
+    with open('data/' + file_name, 'w') as names_file:
+        names_file.write(data)
+
+
 if __name__ == '__main__':
     cleared_names = clear_names('names.txt')
 
-    print(filter_russian_names(cleared_names))
-
+    filtered_names = filter_russian_names(cleared_names)
+    save_to_file('russian_names.txt', '\n'.join(filtered_names))
 
