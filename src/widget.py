216fe44f6e962fda026081e4391afa060da86b21
Nataliia Novozhilova <belka1972@inbox.ru>
2025-12-06 23:36:08 +0300

    add function get_date

--- src/widget.py
+++ src/widget.py
@@ -1,5 +1,6 @@
 from masks import get_mask_card_number
 from masks import get_mask_account
+from datetime import datetime
 
 def mask_account_card(account_card: str) -> str:
     """Функция принимает в качестве аргумента строку, содержащую тип и номер карты или счета, и возвращает строку с замаскированным номером"""
@@ -10,10 +11,22 @@ def mask_account_card(account_card: str) -> str:
         return f"{account_card_info[0]} {get_mask_card_number(account_card_info[1])}"
 
 
+def get_date(date: str) -> str:
+    """Функция принимает на вход строку в формате 2024-03-11Т02:26:18.671407 и возвращает строку в формате ДД.ММ.ГГГГ"""
+    current_date = datetime.strptime(date, "%Y-%m-%dT%H:%M:%S.%f")
+    return current_date.strftime("%d.%m.%Y")
+
+
 if __name__ == "__main__":
     test_1 = "Visa Platinum 8990922113665229"
     test_2 = "Счет 73654108430135874305"
     print(mask_account_card(test_1))
     print(mask_account_card(test_2))
+
+    test_date = "2024-03-11T02:26:18.671407"
+    print(get_date(test_date))
+
+
+
 
 
