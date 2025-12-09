03e2f29074863d63c8e43aeee91aa8164f079bc4
Nataliia Novozhilova <belka1972@inbox.ru>
2025-12-09 22:45:32 +0300

    update .gitignore

--- src/widget.py
+++ src/widget.py
@@ -1,9 +1,10 @@
-from masks import get_mask_card_number
-from masks import get_mask_account
 from datetime import datetime
 
+from masks import get_mask_account, get_mask_card_number
+
+
 def mask_account_card(account_card: str) -> str:
-    """Функция принимает в качестве аргумента строку, содержащую тип и номер карты или счета, и возвращает строку с замаскированным номером"""
+    """Функция принимает строку c типом и номером карты или счета, и возвращает строку с замаскированным номером"""
     account_card_info = account_card.rsplit(' ', 1)
     if "Счет" in account_card:
         return f"{account_card_info[0]} {get_mask_account(account_card_info[1])}"
@@ -12,7 +13,7 @@ def mask_account_card(account_card: str) -> str:
 
 
 def get_date(date: str) -> str:
-    """Функция принимает на вход строку в формате 2024-03-11Т02:26:18.671407 и возвращает строку в формате ДД.ММ.ГГГГ"""
+    """Функция принимает строку в формате 2024-03-11Т02:26:18.671407 и возвращает строку в формате ДД.ММ.ГГГГ"""
     current_date = datetime.strptime(date, "%Y-%m-%dT%H:%M:%S.%f")
     return current_date.strftime("%d.%m.%Y")
 
@@ -25,8 +26,3 @@ if __name__ == "__main__":
 
     test_date = "2024-03-11T02:26:18.671407"
     print(get_date(test_date))
-
-
-
-
-
