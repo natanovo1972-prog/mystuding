7a26fb66dbddd2c3227292a554eb2caa16884758
Nataliia Novozhilova <belka1972@inbox.ru>
2026-03-15 17:10:33 +0300

    add logging modules utils.py and masks.py; add directory logs

--- src/utils.py
+++ src/utils.py
@@ -1,18 +1,34 @@
 import json
+import logging
 import os
+
+Base_dir = os.path.dirname(os.path.abspath(__file__))  # Получаем путь к папке src, где лежит модуль utils
+Log_dir = os.path.join(Base_dir, "..", "logs")  # Поднимаемся на уровень выше в корне проекта и находим там папку logs
+log_file_path = os.path.join(Log_dir, "utils.log")  # Соединяем путь к папке с именем файла
+
+logger = logging.getLogger("utils")
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler(log_file_path, encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s) - %(name)s - %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 
 def fin_transaction(filepath: str) -> list[dict]:
     """Функция приинмает путь до JSON-файла и возвращает список словарей
        с данными о финансовых транзакциях"""
     try:
+        logger.debug(f"Загружаем данные из {filepath}")
         with open(filepath, "r", encoding="utf-8") as file:
             fin_trans = json.load(file)
-    except json.JSONDecodeError:  # В файле ошибка
+    except json.JSONDecodeError as e:  # В файле ошибка
+        logger.error(f"Ошибка в формате JSON: {e}")
         return []
     except FileNotFoundError:  # Файл не найден
+        logger.error(f"Файл не найден по пути: {filepath}")
         return []
     if not isinstance(fin_trans, list):  # Файл не является списком
+        logger.error(f"Данные файла не являются списком")
         return []
 
     return fin_trans
