59c91280f967539786292bd60cc08818f2881929
Nataliia Novozhilova <belka1972@inbox.ru>
2026-07-08 21:49:40 +0300

    update modules product.py, category.py, test_product.py, test_category.py and html report

--- tests/test_category.py
+++ tests/test_category.py
@@ -8,10 +8,27 @@ def test_category_init(category_exemplar):
         "Смартфоны, как средство не только коммуникации,"
         "но и получения дополнительных функций для удобства жизни"
     )
-    assert len(category_exemplar.products) == 2
+    assert "Iphone 15" in category_exemplar.products
+    assert "Xiaomi Redmi Note 11" in category_exemplar.products
 
 
 def test_category_count(category_exemplar, category_attribute):
     """Тестируем, что счетчики класса правильно суммируют данные двух атрибутов"""
     assert Category.category_count == 2
     assert Category.product_count == 5
+
+
+def test_add_product(category_exemplar, product_exemplar):
+    """Тестируем добавление нового продукта в категорию"""
+    category_exemplar.add_product(product_exemplar)
+    assert Category.product_count == 3
+    assert "Samsung Galaxy S23 Ultra" in category_exemplar.products
+
+
+def test_category_products_format(category_exemplar):
+    """Тестируем, что свойства продукта выводятся в нужном формате"""
+    expected_format = (
+        "Iphone 15, 210000.0 руб. Остаток 8 шт.\n"
+        "Xiaomi Redmi Note 11, 31000.0 руб. Остаток 14 шт."
+    )
+    assert category_exemplar.products == expected_format
--- tests/test_product.py
+++ tests/test_product.py
@@ -1,3 +1,6 @@
+from src.product import Product
+from tests.conftest import product_exemplar
+
 
 def test_product_init(product_exemplar):
     """Тестируем инициализацию экземпляров класса Продукты"""
@@ -5,3 +8,27 @@ def test_product_init(product_exemplar):
     assert product_exemplar.description == "256GB, Серый цвет, 200MP камера"
     assert product_exemplar.price == 180000.0
     assert product_exemplar.quantity == 5
+
+
+def test_new_product():
+    """Тестируем создание продукта через класс-метод new_product"""
+    product_data = {
+        "name": "Samsung",
+        "description": "Смартфон",
+        "price": 80000.0,
+        "quantity": 5
+    }
+    product_new = Product.new_product(product_data)
+    assert product_new.name == "Samsung"
+    assert product_new.description == "Смартфон"
+    assert product_new.price == 80000.0
+    assert product_new.quantity == 5
+
+
+def test_price_setter(product_exemplar):
+    """Тестируем изменение цены через сеттер"""
+    product_exemplar.price = 15000.0
+    assert product_exemplar.price == 15000.0
+
+    product_exemplar.price = -15000.0
+    assert product_exemplar.price == 15000.0
